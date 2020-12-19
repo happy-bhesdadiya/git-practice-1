4939d130ef3b339b21561afab11b7153eb412985
Happy Bhesdadiya <happy.bhesdadiya@bacancy.com>
2020-12-19 19:24:46 +0530

    Add thirld line in index.js & Add second line in utils.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log('Hello this is index.js File');
 console.log('Appeding a line!');
+console.log('Hello World!');
--- utils.js
+++ utils.js
@@ -1,1 +1,2 @@
 console.log('Creating a utils.js');
+console.log('This is Wonderfull Day!');
